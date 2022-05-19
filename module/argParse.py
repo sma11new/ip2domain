fa7f90b02d7987f6aaef8b2f893127ae6ea092a1
野原新之助 <53944964+Sma11New@users.noreply.github.com>
2022-05-19 10:13:36 +0800

    修改默认请求时延2秒为3秒，减少pageError

--- module/argParse.py
+++ module/argParse.py
@@ -11,7 +11,7 @@ def parseArgs():
     parser = ArgumentParser()
     parser.add_argument("-t", "--target", required=False, type=str, help=f"Target ip/domain")
     parser.add_argument("-f", "--file", dest="file", required=False, type=str, default="", help=f"Target ip/domain file")
-    parser.add_argument("-s", "--delay", dest="delay", required=False, type=int, default=2, help=f"Request delay (default 2s)")
+    parser.add_argument("-s", "--delay", dest="delay", required=False, type=int, default=3, help=f"Request delay (default 3s)")
     parser.add_argument("-T", "--Timeout", dest="timeout", required=False, type=int, default=3, help="Request timeout (default 3s)")
     parser.add_argument("-r", "--rank", required=False, type=int, default=0, help="Show baiduRank size (default 0)")
     parser.add_argument("-o", "--output", dest="output", required=False, type=str, default=f"{date}", help="output file (default ./output/ip2domain_{fileName}_{date}.csv)")
